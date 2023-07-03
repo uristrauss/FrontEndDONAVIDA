56cf627550c5bae445eea537155961c6bf7939b3
uristrauss <uriel.strauss@gmail.com>
2023-07-03 10:24:19 -0300

    login

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>BENEFICIARIOS</h1>
-        <Login/>
+        <RegistroBeneficiario/>
       </div>
     </div>
   );
